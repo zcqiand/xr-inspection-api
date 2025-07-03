50eef78ef8928337390b981e38e2ce255fd7d099
zcqiand <zcqiand@163.com>
2025-07-03 21:16:38 +0800

    refactor: 删除待办事项功能
    - 移除 TodoController、TodoCreateDto、TodoResponseDto、TodoUpdateDto、Todo 模型、TodoRepository 和 TodoService
    - 删除 http-test/todos.rest 文件

--- src/main/java/com/nanrong/inspection/repository/TodoRepository.java
+++
@@ -1,9 +0,0 @@
-package com.nanrong.inspection.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import com.nanrong.inspection.model.Todo;
-
-@Repository
-public interface TodoRepository extends JpaRepository<Todo, Long> {
-}
